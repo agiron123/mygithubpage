3dbcf3e66fdaabe56bb6bc199285cf7173ef6f03
Andre Giron <agiron123@gmail.com>
2025-11-01 20:49:57 -0700

    tidying things up and moving around

--- astro-site copy/src/config/site.ts
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  title: "Andre S. Giron",
-  description: "Made with <3 in San Francisco, CA",
-  email: "agiron123@gmail.com",
-  socials: {
-    github: "agiron123",
-    twitter: "agiron_78",
-  },
-};
