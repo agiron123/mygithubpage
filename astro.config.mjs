41de89163dd129318e9a75f338b06886435f33d5
Andre Giron <agiron123@gmail.com>
2026-02-16 00:22:19 -0800

    gh-pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "http://andregiron.me",
+  site: "https://andregiron.me",
   vite: {
     css: {
       preprocessorOptions: {
