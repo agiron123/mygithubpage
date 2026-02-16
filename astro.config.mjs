e61d98ca1dd5d1f7ac231e0072697cc71ad85f2b
Andre Giron <agiron123@gmail.com>
2026-02-15 23:52:51 -0800

    Fix up more deprecation warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "http://andregiron.me",
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
 });
